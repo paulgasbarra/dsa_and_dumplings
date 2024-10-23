c12950e42ba037d3cd9f41be30012d7bcd8f1060
Paul Gasbarra <paulgasbarra@Admins-MacBook-Air.local>
2024-10-23 14:48:09 -0400

    more instructions

--- sliding_window/max_sum_subarray.js
+++ sliding_window/max_sum_subarray.js
@@ -1,3 +1,14 @@
+/* Instructions: Implement a function that returns the maximum sum of a subarray of size k.
+    Example: 
+        Input: [1, 2, 3, 4, 5, 6, 7], k = 3
+        Output: 18
+        Explanation: The maximum sum of a contiguous subarray of size 3 is 18, which is the sum of the last three elements.
+        
+        Input: [4, 21, 11, 16, -26, 8, 13, -4, 6], k = 4
+        Output: 52
+        Explanation: The maximum sum of a contiguous subarray of size 4 is 52.
+*/
+
 const maxSumSubarray = (arr, k) => {};
 
 module.exports = maxSumSubarray;
--- two_pointer/container_with_most_water.js
+++ two_pointer/container_with_most_water.js
@@ -1,3 +1,12 @@
+/** Instructions:
+    Implement a function that returns the maximum area of water that can be contained between two lines.
+    The input is an array of integers where each integer represents the height of a line.
+    Example:
+        * Input: [1, 8, 6, 2, 5, 4, 8, 3, 7]
+        * Output: 49
+        * Explanation: The maximum area of water contained by the container is 49, which is formed between the lines at index 1 and index 8.
+ */
+
 function containerWithMostWater(arr) {}
 
 module.exports = containerWithMostWater;
